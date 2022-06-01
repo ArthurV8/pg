b2f3d02cd655ed7455b08ac9da41b1525964ad78
ArthurV8 <93012457+ArthurV8@users.noreply.github.com>
2022-06-01 13:53:55 -0300

    01/06/2022

--- sketch.js
+++ sketch.js
@@ -7,9 +7,9 @@ var grama=0.05;
 var voo;
 var r2,l2;
 var piso;
-var gasol=100;
-
-
+var gasol=25;
+var navep;
+var naves;
 
 function preload(){
   areia=loadImage("./img/bg_sur.png");
@@ -17,6 +17,7 @@ function preload(){
   voo=loadAnimation("./img/b_thrust_1.png","./img/b_thrust_2.png","./img/b_thrust_3.png");
   r2=loadAnimation("./img/right_thruster_1.png","./img/right_thruster_2.png");
   l2=loadAnimation("./img/left_thruster_1.png","./img/left_thruster_2.png");
+  navep=loadImage("./img/lz.png");
   r2.looping=false;
   l2.looping=false;
   voo.playing=true;
@@ -33,24 +34,45 @@ function setup() {
   donag.addAnimation("r2",r2);
   donag.addAnimation("l2",l2);
   piso=createSprite(500,590,1000,20);
+  naves=createSprite(800,500,50,30);
+  naves.addImage(navep);
+  naves.scale=0.3;
+  naves.debug=false;
+  naves.setCollider("rectangle",0,180,400,100);
 }
 
-function draw() {
+function draw(){
   background("black");
   image(areia,0,0,1200,810);
-  drawSprites(); 
+  
   
   vyl=vyl+grama;
   donag.position.y+=vyl;
   donag.position.x+=vxl;
 
+  drawSprites();
   textSize(25);
   fill("#87CEEB");
   text("vyl="+Math.round(vyl),850,30);
   text("vxl="+Math.round(vxl),850,60);
   text("gasol="+gasol,850,90);
 
-  donag.collide(piso)
+  if(donag.isTouching(piso)){
+    vyl=0;
+    vxl=0;
+  }
+  if(donag.isTouching(naves)){
+    text("boaaa:/",500,500);
+    vyl=0;
+    vxl=0;
+    donag.position.y=495;
+    console.log(donag.position.y);
+  }
+  if(gasol===0){
+    donag.destroy();
+    fill("white")
+    text("tenta outra vez",500,300);
+  }
 }
 function keyPressed(){
   if(keyCode===UP_ARROW&&gasol>0){
